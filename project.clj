fcb52225019cdcec0001cbfd1a7a314801117276
Aaron Diep <aaron@delite.ca>
2018-09-18 18:18:35 -0600

    deploy to clojars

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.clojars.cloggo/restify "0.1.0"
+  :description "clojurescript API to access javascript restify library package"
+  :url "https://github.com/cloggo/cljs-restify"
+  :license {:name "MIT" }
+  :source-paths ["src"]
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ test/restify/core_test.clj
@@ -0,0 +1,7 @@
+(ns restify.core-test
+  (:require [clojure.test :refer :all]
+            [restify.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
